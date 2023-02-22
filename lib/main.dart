d6a8613ce449e695099d47e8979f274318ff559b
pashabetenya <pavelbiatsenia@gmail.com>
2023-02-22 23:47:28 +0300

    added packages, and loaded pictures. pulled up all the dependencies. prescibed the folder and file structure.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,1 @@
-import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const Application());
-}
-
-class Application extends StatelessWidget {
-  const Application({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp();
-  }
-}
